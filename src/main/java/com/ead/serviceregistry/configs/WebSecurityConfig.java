22ebc7eff9772a8003fc83d06a24ccc03b6f4c49
Guilherme <guilhermesantos.srs@gmail.com>
2026-09-02 14:39:48 -0300

    Implementar Basic Authentication no Service Registry.

---
+++ src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.ead.serviceregistry.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Value("${ead.serviceRegistry.username}")
+    private String username;
+
+    @Value("${ead.serviceRegistry.password}")
+    private String password;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
